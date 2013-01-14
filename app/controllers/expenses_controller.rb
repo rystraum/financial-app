9294330bec504227eec2c1a17040850c8f281d77
oobgam <rdmagboo@gmail.com>
2013-01-15 00:36:32 +0800

    Refactored code

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,23 +1,24 @@
 class ExpensesController < ApplicationController
 
+	before_filter :find_project, except: [:destroy]
+	before_filter :find_expense, except: [:create, :new]
+	before_filter :new_expense, only: [:create, :new]
+	before_filter :find_expenses, only: [:index]
+
 	def index
-		@project = Project.find params[:id]
-		@expenses = @project.expenses.all
+		@expenses
 	end
 
 	def show
-		@project = Project.find params[:id]
-		@expense = @project.expenses.find params[:id]
+		@expense
 	end
 
 	def new
-		@project = Project.find params[:id]
-		@expense = @project.expenses.build params[:expense]
+		@expense
 	end
 
 	def create
-		@project = Project.firnd params[:id]
-		@expense = @project.expenses.new params[:expense]
+		@expense
 		if @expense.save
 			redirect_to @project
 		else
@@ -32,9 +33,25 @@ class ExpensesController < ApplicationController
 	end
 
 	def destroy
-		@expense = Expense.find params[:id]
 		@expense.destroy
 		redirect_to :back
+	end
+
+	private
+	def find_project
+		@project = Project.find params[:id]
+	end
+
+	def find_expenses
+		@expenses = @project.expenses.all
+	end
+
+	def find_expense
+		@expense = Expense.find params[:id]
+	end
+
+	def new_expense
+		@expense = @project.expenses.new params[:expense]
 	end
 
 end
